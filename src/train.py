3451fdb973e72ef7fce991041b95d5276f3bf0b4
sinwook <nansw07@naver.com>
2026-05-07 20:52:19 +0900

    Update train.py

--- src/train.py
+++ src/train.py
@@ -44,59 +44,63 @@ def train_and_test(
                 )
     epoch+=1
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+def main():
+  device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-# =========================
-# Get dataloader
-# =========================
-train_dataloader, test_dataloader = get_cifar10_dataloaders(batch_size=128, root='./data')
+  # =========================
+  # Get dataloader
+  # =========================
+  train_dataloader, test_dataloader = get_cifar10_dataloaders(batch_size=128, root='./data')
 
-# =========================
-# Model select
-# =========================
-ResNet44=ResNet(N=7).to(device)
-ResNet44.apply(init_weights)
-#PlainNet44=PlainNet(N=7).to(device)
-#PlainNet44.apply(init_weights)
+  # =========================
+  # Model select
+  # =========================
+  ResNet44=ResNet(N=7).to(device)
+  ResNet44.apply(init_weights)
+  #PlainNet44=PlainNet(N=7).to(device)
+  #PlainNet44.apply(init_weights)
 
-model = ResNet44
-#model = PlainNet44
+  model = ResNet44
+  #model = PlainNet44
 
-# =========================
-# Accuracy and loss list
-# =========================
-Res_train_acc_list = []
-Res_test_acc_list = []
-Res_train_loss_list = []
+  # =========================
+  # Accuracy and loss list
+  # =========================
+  Res_train_acc_list = []
+  Res_test_acc_list = []
+  Res_train_loss_list = []
 
-#Plain_train_acc_list = []
-#Plain_test_acc_list = []
-#Plain_train_loss_list = []
+  #Plain_train_acc_list = []
+  #Plain_test_acc_list = []
+  #Plain_train_loss_list = []
 
-# =========================
-# Training conditions
-# =========================
-loss_fn = nn.CrossEntropyLoss()
-epochs=[20,15,15]
-lrs=[1e-3,1e-4,1e-4]
-weight_decays =[1e-3,1e-3,1e-3]
-optimizers=[Adam,Adam,SGD]
+  # =========================
+  # Training conditions
+  # =========================
+  loss_fn = nn.CrossEntropyLoss()
+  epochs=[20,15,15]
+  lrs=[1e-3,1e-4,1e-4]
+  weight_decays =[1e-3,1e-3,1e-3]
+  optimizers=[Adam,Adam,SGD]
 
-# =========================
-# Train and test
-# =========================
-train_and_test(model=model,
-    train_dataloader=train_dataloader,
-    test_dataloader=test_dataloader,
-    loss_fn=loss_fn,
-    train_acc_list=Res_train_acc_list,
-    train_loss_list=Res_train_loss_list,
-    test_acc_list=Res_test_acc_list,
-    #train_acc_list=Plain_train_acc_list,
-    #train_loss_list=Plain_train_loss_list,
-    #test_acc_list=Plain_test_acc_list,
-    device=device,
-    optimizers=optimizers,
-    epochs=epochs,
-    lrs=lrs,
-    weight_decays=weight_decays)
+  # =========================
+  # Train and test
+  # =========================
+  train_and_test(model=model,
+      train_dataloader=train_dataloader,
+      test_dataloader=test_dataloader,
+      loss_fn=loss_fn,
+      train_acc_list=Res_train_acc_list,
+      train_loss_list=Res_train_loss_list,
+      test_acc_list=Res_test_acc_list,
+      #train_acc_list=Plain_train_acc_list,
+      #train_loss_list=Plain_train_loss_list,
+      #test_acc_list=Plain_test_acc_list,
+      device=device,
+      optimizers=optimizers,
+      epochs=epochs,
+      lrs=lrs,
+      weight_decays=weight_decays)
+
+if __name__ == "__main__":
+    main()
